5f917f5e7253bc4f37f5e5da59b6384601b68789
Joshua Scott <joshua.scott@gmail.com>
2018-02-19 22:28:41 -0600

    add ex_doc

--- mix.exs
+++ mix.exs
@@ -23,7 +23,9 @@ defmodule Lzf.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0"}
+    ]
   end
 
   defp description do
