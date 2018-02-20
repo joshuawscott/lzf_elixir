01ca1d0ef4eeb766020e29afdd5d05c7272387f6
Joshua Scott <joshua.scott@gmail.com>
2018-02-19 22:04:05 -0600

    mix new

---
+++ lib/lzf.ex
@@ -0,0 +1,18 @@
+defmodule Lzf do
+  @moduledoc """
+  Documentation for Lzf.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lzf.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lzf_test.exs
@@ -0,0 +1,8 @@
+defmodule LzfTest do
+  use ExUnit.Case
+  doctest Lzf
+
+  test "greets the world" do
+    assert Lzf.hello() == :world
+  end
+end
