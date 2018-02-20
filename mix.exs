fa0cd5fb1dde3c04617362db2b72143b315365f1
Joshua Scott <joshua.scott@gmail.com>
2018-02-19 23:09:43 -0600

    ex_doc only in dev

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Lzf.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0"}
+      {:ex_doc, ">= 0.0.0", only: [:dev]}
     ]
   end
 
