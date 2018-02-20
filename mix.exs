650063e002d0bba62a7040056cb0f9136a433a34
Joshua Scott <joshua.scott@gmail.com>
2018-02-19 23:17:25 -0600

    bump to 0.1.2
    remove ex_doc from prod deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lzf.MixProject do
   def project do
     [
       app: :lzf,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
